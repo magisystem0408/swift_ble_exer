e12689a1a65200e1211abd46becf6dc3d8e148b1
magisystem0408 <masato.matsudo@gmail.com>
2024-06-16 18:56:28 +0900

    Initial Commit

---
+++ core-ble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  core-ble
+//
+//  Created by 松戸誠人 on 2024/06/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ core-ble/core_bleApp.swift
@@ -0,0 +1,17 @@
+//
+//  core_bleApp.swift
+//  core-ble
+//
+//  Created by 松戸誠人 on 2024/06/16.
+//
+
+import SwiftUI
+
+@main
+struct core_bleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
