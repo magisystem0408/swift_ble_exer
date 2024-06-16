b978a3381c529048291ea7d9ebf9ed961d655180
magisystem0408 <masato.matsudo@gmail.com>
2024-06-16 20:21:18 +0900

    my first swift commit

--- core-ble/ContentView.swift
+++ core-ble/ContentView.swift
@@ -6,16 +6,44 @@
 //
 
 import SwiftUI
+import CoreBluetooth
+
+class BluetoothViewModel: NSObject, ObservableObject {
+    private var centralManager: CBCentralManager?
+    private var peripherals: [CBPeripheral]=[]
+    @Published var peripheralNames:[String] = []
+    
+    override init() {
+        super.init()
+        self.centralManager = CBCentralManager(delegate: self, queue: .main)
+    }
+}
+
+extension BluetoothViewModel: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn{
+            self.centralManager?.scanForPeripherals(withServices: nil)
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any],rssi RSSI: NSNumber) {
+        if !peripherals.contains(peripheral){
+            self.peripherals.append(peripheral)
+            self.peripheralNames.append(peripheral.name ?? "unnamed device")
+        }
+    }
+}
 
 struct ContentView: View {
+    @ObservedObject private var bluetoothViewModel = BluetoothViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(bluetoothViewModel.peripheralNames,id: \.self){
+                peripheral in Text(peripheral)
+            }
+            .navigationTitle("Peripherals")
         }
-        .padding()
     }
 }
 
