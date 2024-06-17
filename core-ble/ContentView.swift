bf2ada65782b50c51fc1382ea56f10fe4b97a75f
magisystem0408 <masato.matsudo@gmail.com>
2024-06-17 15:03:39 +0900

    feat: add ble class

--- core-ble/ContentView.swift
+++ core-ble/ContentView.swift
@@ -8,42 +8,60 @@
 import SwiftUI
 import CoreBluetooth
 
-class BluetoothViewModel: NSObject, ObservableObject {
-    private var centralManager: CBCentralManager?
-    private var peripherals: [CBPeripheral]=[]
-    @Published var peripheralNames:[String] = []
-    
-    override init() {
-        super.init()
-        self.centralManager = CBCentralManager(delegate: self, queue: .main)
-    }
-}
 
-extension BluetoothViewModel: CBCentralManagerDelegate {
+class BluetoothManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate{
+    private var centeralManager: CBCentralManager!
+    private var peripheralToConnect: CBPeripheral?
+    var onDeviceDiscoverd: ((CBPeripheral) -> Void)?
+    
+    override init(){
+        super.init()
+        centeralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         if central.state == .poweredOn{
-            self.centralManager?.scanForPeripherals(withServices: nil)
+            centeralManager.scanForPeripherals(withServices: nil)
+        }else{
+            print("Bluetooth is not avaliable.")
         }
     }
     
-    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any],rssi RSSI: NSNumber) {
-        if !peripherals.contains(peripheral){
-            self.peripherals.append(peripheral)
-            self.peripheralNames.append(peripheral.name ?? "unnamed device")
-        }
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        onDeviceDiscoverd?(peripheral)
+    }
+    
+    //NOTE: device発見した時
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+    }
+    
+    func centralManager(_ central: CBCentralManager, willRestoreState dict: [String : Any]) {
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: (any Error)?) {
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: (any Error)?) {
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, timestamp: CFAbsoluteTime, isReconnecting: Bool, error: (any Error)?) {
     }
 }
 
 struct ContentView: View {
-    @ObservedObject private var bluetoothViewModel = BluetoothViewModel()
-    
+    @State private var devices:[CBPeripheral] = []
+    private var bluetoothManager = BluetoothManager()
     var body: some View {
-        NavigationView {
-            List(bluetoothViewModel.peripheralNames,id: \.self){
-                peripheral in Text(peripheral)
+        List(devices, id: \.identifier){
+            device in Text(device.name ?? "Unknown device")
+            Text(device.identifier.uuidString)
+            
+        }.navigationTitle("BLE Device")
+            .onAppear{
+                bluetoothManager.onDeviceDiscoverd = {device in
+                    self.devices.append(device)
+                }
             }
-            .navigationTitle("Peripherals")
-        }
     }
 }
 
