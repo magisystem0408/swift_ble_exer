54ed40c7babe82191f70f394d5bed36ea3127517
magisystem0408 <masato.matsudo@gmail.com>
2025-03-04 00:37:27 +0900

    feat: add ble manager

--- core-ble/ContentView.swift
+++ core-ble/ContentView.swift
@@ -8,44 +8,88 @@
 import SwiftUI
 import CoreBluetooth
 
-class BluetoothViewModel: NSObject, ObservableObject {
-    private var centralManager: CBCentralManager?
-    private var peripherals: [CBPeripheral]=[]
-    @Published var peripheralNames:[String] = []
+class BluetoothManager: NSObject, ObservableObject {
+    @Published var peripherals: [CBPeripheral] = []
+    @Published var connectedPeripheral: CBPeripheral?
+    @Published var data: Data?
+    
+    private var centralManager: CBCentralManager!
     
     override init() {
         super.init()
-        self.centralManager = CBCentralManager(delegate: self, queue: .main)
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
 }
 
-extension BluetoothViewModel: CBCentralManagerDelegate {
+extension BluetoothManager: CBCentralManagerDelegate, CBPeripheralDelegate {
+    
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        //NOTE: state controll.
         if central.state == .poweredOn{
-            self.centralManager?.scanForPeripherals(withServices: nil)
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
         }
     }
     
-    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any],rssi RSSI: NSNumber) {
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        //NOTE: if you discover peripheral device, this func invoked.
+        
         if !peripherals.contains(peripheral){
-            self.peripherals.append(peripheral)
-            self.peripheralNames.append(peripheral.name ?? "unnamed device")
+            peripherals.append(peripheral)
         }
+    }
+    
+    func connect(to peripheral: CBPeripheral){
+        centralManager.stopScan()
+        centralManager.connect(peripheral,options: nil)
+        connectedPeripheral = peripheral
+        connectedPeripheral?.delegate = self
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        //NOTE: if you connect peripheral device, this func invoked.
+        peripheral.discoverServices(nil)
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: (any Error)?) {
+        guard let services = peripheral.services else {return}
+        for service in services {
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: (any Error)?) {
+        guard let characteristics = service.characteristics else { return }
+                for characteristic in characteristics {
+                    peripheral.readValue(for: characteristic)
+            }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: (any Error)?) {
+        data = characteristic.value
     }
 }
 
 struct ContentView: View {
-    @ObservedObject private var bluetoothViewModel = BluetoothViewModel()
-    
+    @ObservedObject var bluetoothManager = BluetoothManager()
+
     var body: some View {
         NavigationView {
-            List(bluetoothViewModel.peripheralNames,id: \.self){
-                peripheral in Text(peripheral)
+            List(bluetoothManager.peripherals, id: \.identifier) { peripheral in
+                Button(action: {
+                    bluetoothManager.connect(to: peripheral)
+                }) {
+                    Text(peripheral.name ?? "Unknown")
+                }
             }
-            .navigationTitle("Peripherals")
+            .navigationTitle("Bluetooth Devices")
         }
+        .onAppear {
+            bluetoothManager.centralManagerDidUpdateState(bluetoothManager.centralManager)
+        }
+        .alert("Text", isPresented: false)
     }
 }
+
 
 #Preview {
     ContentView()
